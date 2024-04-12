65dd7301162e0617da955137b4bcaec8050b27c5
Roberto Capancioni <roberto.capancioni@gmail.com>
2024-04-12 15:00:56 +0200

    Update d08_join.sql

--- D10/d08_join.sql
+++ D10/d08_join.sql
@@ -7,3 +7,19 @@ select p.ID,
        p.PREZZO_VENDITA
   from D08_PRODOTTO p
   join D08_TIPO_PRODOTTO tp on p.TIPO_PRODOTTO_ID = tp.ID;
+
+
+select v.id,
+       v.cliente_id,
+       c.cliente,
+       c.zona,
+       v.prodotto_id,
+       p.prodotto,
+       p.tipo_prodotto_id,
+       tp.tipo_prodotto,
+       v.data,
+       v.quantita
+  from d08_vendita v
+  join d08_cliente c on v.cliente_id = c.id 
+  join d08_prodotto p on v.prodotto_id = p.id
+  join d08_tipo_prodotto tp on p.tipo_prodotto_id = tp.id;
