2d5b8d944fb46ac3036764f1736ba27e6f092009
Roberto Capancioni <roberto.capancioni@gmail.com>
2024-04-12 14:55:17 +0200

    Update d08_join.sql

--- D10/d08_join.sql
+++ D10/d08_join.sql
@@ -1,1 +1,9 @@
-.
+select p.ID,
+       p.TIPO_PRODOTTO_ID,
+       tp.TIPO_PRODOTTO,
+       tp.GRUPPO,
+       p.PRODOTTO,
+       p.PREZZO_ACQUISTO,
+       p.PREZZO_VENDITA
+  from D08_PRODOTTO p
+  join D08_TIPO_PRODOTTO tp on p.TIPO_PRODOTTO_ID = tp.ID
