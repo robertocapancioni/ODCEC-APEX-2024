fcfd47f6f2ad71ebb2890980ec96f361ce0d557b
Roberto Capancioni <roberto.capancioni@gmail.com>
2024-04-15 15:17:01 +0200

    Update d11_select_azione_dinamica.sql

--- D11/d11_select_azione_dinamica.sql
+++ D11/d11_select_azione_dinamica.sql
@@ -2,10 +2,10 @@
 select nvl(:P13_ORE,0)*r.tariffa_oraria 
   from d11_persona p
   join d11_ruolo r on p.ruolo_id = r.id
-   and p.id = :P13_PERSONA_ID
+ where p.id = :P13_PERSONA_ID
 
 -- nella griglia interattiva
 select nvl(:ORE,0)*r.tariffa_oraria 
   from d11_persona p
   join d11_ruolo r on p.ruolo_id = r.id
-   and p.id = :PERSONA_ID
+ where p.id = :PERSONA_ID
