017bdf08bc2326b0cb646efab6de998f27cb48ce
Roberto Capancioni <roberto.capancioni@gmail.com>
2024-04-19 15:42:16 +0200

    Update D12_query_appuntamento.sql

--- D12/D12_query_appuntamento.sql
+++ D12/D12_query_appuntamento.sql
@@ -11,4 +11,21 @@ select
  left join D12_TIPO_APPUNTAMENTO ta
    on a.TIPO_APPUNTAMENTO_ID = ta.TIPO_APPUNTAMENTO_ID
  left join D12_NOME n
-   on a.NOME_ID = n.NOME_ID
+   on a.NOME_ID = n.NOME_ID;
+
+
+select 
+      a.ID,
+      a.DATA_INIZIO,
+      a.DATA_FINE,
+      a.DESCRIZIONE,
+      a.TIPO_APPUNTAMENTO_ID,
+     ta.TIPO_APPUNTAMENTO, 
+      a.NOME_ID,
+      n.NOME,
+      a.DESCRIZIONE||' - '||n.NOME||' - '||ta.TIPO_APPUNTAMENTO DESCRIZIONE_EXT
+ from D12_APPUNTAMENTO a
+ left join D12_TIPO_APPUNTAMENTO ta
+   on a.TIPO_APPUNTAMENTO_ID = ta.TIPO_APPUNTAMENTO_ID
+ left join D12_NOME n
+   on a.NOME_ID = n.NOME_ID;
