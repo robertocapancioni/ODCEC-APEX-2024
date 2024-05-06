b9dc3c27e15559c6a26e924b82f9115f2efbf1d0
Roberto Capancioni <roberto.capancioni@gmail.com>
2024-05-06 13:55:29 +0200

    Create d14_select_chart.sql

---
+++ D14/d14_select_chart.sql
@@ -0,0 +1,31 @@
+with acquisto_grp as(
+  select prodotto_id,prodotto,
+         to_char(data,'YYYY') anno,
+         to_char(data,'YYYY-MM') anno_mese,
+         sum(quantita)quantita, 
+         sum(valore)valore 
+    from x01_acquisto_vw
+   group by prodotto_id,prodotto,to_char(data,'YYYY'),to_char(data,'YYYY-MM')
+)
+ select v.*,
+  sum(quantita) over (partition by prodotto_id order by anno_mese) quantita_prog,
+  sum(valore) over (partition by prodotto_id order by anno_mese) valore_prog
+  from acquisto_grp v
+  where prodotto_id = :P18_ID
+  order by anno_mese;
+
+  with vendita_grp as(
+  select prodotto_id,prodotto,
+         to_char(data,'YYYY') anno,
+         to_char(data,'YYYY-MM') anno_mese,
+         sum(quantita)quantita, 
+         sum(valore)valore 
+    from x01_vendita_vw
+   group by prodotto_id,prodotto,to_char(data,'YYYY'),to_char(data,'YYYY-MM')
+)
+ select v.*,
+  sum(quantita) over (partition by prodotto_id order by anno_mese) quantita_prog,
+  sum(valore) over (partition by prodotto_id order by anno_mese) valore_prog
+  from vendita_grp v
+  where prodotto_id = :P18_ID
+  order by anno_mese;
