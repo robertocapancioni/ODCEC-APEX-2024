b93270ebe913cd6184def7d2614e1c140ed230d2
Roberto Capancioni <roberto.capancioni@gmail.com>
2024-07-05 14:31:45 +0200

    Add files via upload

---
+++ D23/d23 insert.sql
@@ -0,0 +1,5 @@
+insert into d23_approvazione (nome,email,livello) values ('ROBERTO','roberto@email.com',1);
+insert into d23_approvazione (nome,email,livello) values ('MARIA'  ,'maria@email.com'  ,1);
+insert into d23_approvazione (nome,email,livello) values ('CARLO'  ,'carlo@email.com'  ,2);
+insert into d23_approvazione (nome,email,livello) values ('LUISA'  ,'luisa@email.com'  ,2);
+insert into d23_approvazione (nome,email,livello) values ('FRANCA' ,'franca@email.com' ,3);
