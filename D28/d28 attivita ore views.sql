aab40dfdce8a7b217c014f3b81e55b235442273c
Roberto Capancioni <roberto.capancioni@gmail.com>
2024-07-22 16:43:35 +0200

    Update d28 attivita ore views.sql

--- D28/d28 attivita ore views.sql
+++ D28/d28 attivita ore views.sql
@@ -10,16 +10,18 @@ create or replace view d28_ore_vw as
 select o.id,o.data,o.ore,
        (select pt.valore
           from d28_persona_tariffa_vw pt
-         where o.data between pt.data_inizio and nvl(pt.data_fine,o.data))valore,
+         where o.persona_id = pt.persona_id and o.data between pt.data_inizio and nvl(pt.data_fine,o.data))valore,
        (select pt.valore*o.ore 
           from d28_persona_tariffa_vw pt
-         where o.data between pt.data_inizio and nvl(pt.data_fine,o.data))importo,
+         where o.persona_id = pt.persona_id and o.data between pt.data_inizio and nvl(pt.data_fine,o.data))importo,
        o.persona_id,p.cod_persona,p.persona,
+       o.tipo_id,t.cod_tipo,t.tipo,
        o.attivita_id,a.cod_attivita,a.attivita,perc_completamento,
        a.stato_id,s.cod_stato,s.stato,
        a.cliente_id,c.cod_cliente,c.cliente
   from d28_ore o
   join d28_persona p on o.persona_id = p.id
+  join d28_tipo t on o.tipo_id = t.id
   join d28_attivita a on o.attivita_id = a.id
   join d28_stato s on a.stato_id = s.id
   join d28_cliente c on a.cliente_id = c.id;
